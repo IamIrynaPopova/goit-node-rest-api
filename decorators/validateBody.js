807b935eb366a20a91c914c49d019f98f2ccda6e
Iryna Popova <popovaira969@gmail.com>
2023-06-03 18:46:05 +0300

    FIXED: validate

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -1,10 +1,16 @@
+const { isEmpty } = require("lodash");
+
 const { HttpError } = require("../helpers");
 
 const validateBodyOnPut = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
-    if (error) {
+    if (isEmpty(req.body)) {
       next(HttpError(400, "missing fields"));
+    }
+
+    if (error) {
+      next(HttpError(400, `missing required ${error.message} field`));
     } else next();
   };
   return func;
@@ -13,7 +19,7 @@ const validateBodyOnPost = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, "missing required name field"));
+      next(HttpError(400, `missing required ${error.message} field`));
     } else next();
   };
   return func;
