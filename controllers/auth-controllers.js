05d2f0ff6126134e34fdf48394a389083ff5f297
Iryna Popova <popovaira969@gmail.com>
2023-06-12 14:10:46 +0300

    INSTALLED package  bcryptjs

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -3,9 +3,16 @@ const { HttpError } = require("../helpers");
 
 const register = async (req, res, next) => {
   try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw HttpError(409, "Email in use");
+    }
     const newUser = await User.create(req.body);
     res.status(201).json({ email: newUser.email, password: newUser.password });
-  } catch {}
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- middlewares/handleMongooseError.js
+++ middlewares/handleMongooseError.js
@@ -1,5 +1,8 @@
 const handleMongooseError = (error, data, next) => {
-  error.status = 400;
+  const { code, name } = error;
+  console.log(code);
+  console.log(name);
+  error.status = code === 11000 && name === "MongoServerError" ? 409 : 400;
   next();
 };
 
