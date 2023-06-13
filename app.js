7814ab19051eff7f81d522d1a88b14d0b8c36845
Iryna Popova <popovaira969@gmail.com>
2023-06-13 09:54:28 +0300

    ADDED: hash password

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-console.log("Continue");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-const bcrypt = require("bcrypt");
+
 
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
